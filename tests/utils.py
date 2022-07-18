e68ef221d9637020e077a4297568f83a58d1b653
Sai Praneeth M <saipraneeth@aarth.io>
2022-07-18 11:53:56 +0530

    refactor get_kafka_config in test utils

--- tests/utils.py
+++ tests/utils.py
@@ -21,11 +21,11 @@ needs_kafka: MarkDecorator = pytest.mark.skipif(
 def get_kafka_config():
     """To get the kafka configuration from environment variables"""
     return {
-        "bootstrap_server":  os.environ["KAFKA_BOOTSTRAP_SERVER"] if "KAFKA_BOOTSTRAP_SERVER" in os.environ else '',
-        "security_protocol": os.environ["KAFKA_SECURITY_PROTOCOL"] if "KAFKA_SECURITY_PROTOCOL" in os.environ else '',
-        "sasl_mechanisms": os.environ["KAFKA_SASL_MECHANISMS"] if "KAFKA_SASL_MECHANISMS" in os.environ else '',
-        "sasl_username": os.environ["KAFKA_SASL_USERNAME"] if "KAFKA_SASL_USERNAME" in os.environ else '',
-        "sasl_password": os.environ["KAFKA_SASL_PASSWORD"] if "KAFKA_SASL_PASSWORD" in os.environ else ''
+        "bootstrap_server":  os.environ.get('KAFKA_BOOTSTRAP_SERVER', ''),
+        "security_protocol": os.environ.get('KAFKA_SECURITY_PROTOCOL', ''),
+        "sasl_mechanisms": os.environ.get('KAFKA_SASL_MECHANISMS', ''),
+        "sasl_username": os.environ.get('KAFKA_SASL_USERNAME', ''),
+        "sasl_password": os.environ.get('KAFKA_SASL_PASSWORD', '')
     }
 
 
