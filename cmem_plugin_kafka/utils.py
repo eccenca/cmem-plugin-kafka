6f4556bd596fa76b8f2b74353c1b8ac75ee85f50
saipraneeth <2506664+msaipraneeth@users.noreply.github.com>
2026-03-04 12:10:20 +0000

    Fix headers dict comprehension to use .items() instead of iterating over raw headers

--- cmem_plugin_kafka/utils.py
+++ cmem_plugin_kafka/utils.py
@@ -290,7 +290,7 @@ def get_message_with_json_wrapper(message: KafkaMessage) -> str:
     msg_with_wrapper = {"message": {"key": message.key, "content": json.loads(message.value)}}
     if message.headers:
         msg_with_wrapper["message"]["headers"] = {
-            header[0]: header[1] for header in message.headers
+            key: value for key,value in message.headers.items()
         }
     return json.dumps(msg_with_wrapper, cls=BytesEncoder)
 
